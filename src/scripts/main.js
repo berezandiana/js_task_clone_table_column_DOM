59fed01c2f1f871084b9a34a7cb36a858c394651
Diana <berezandiana24@gmail.com>
2025-05-12 13:30:38 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,12 @@ const table = document.querySelector('table');
 const tableRows = [...table.rows];
 
 tableRows.forEach((row) => {
-  const secondCell = row.cells[1];
-  const cloneCell = secondCell.cloneNode(true);
+  if (row.cells.length >= 2) {
+    const secondCell = row.cells[1];
+    const cloneCell = secondCell.cloneNode(true);
 
-  const insertBeforeCell = row.cells[row.cells.length - 1];
+    const insertBeforeCell = row.cells[row.cells.length - 1];
 
-  row.insertBefore(cloneCell, insertBeforeCell);
+    row.insertBefore(cloneCell, insertBeforeCell);
+  }
 });
